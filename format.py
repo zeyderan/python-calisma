3d566eb0a043e13833a28ccbee458a17cc4a7e3e
zeyderan <aykutbayraktar34gmail.com>
2020-04-25 10:56:38 +0300

    string formatlama çalışıldı

---
+++ format.py
@@ -0,0 +1,18 @@
+name = 'Ömer'
+surname = 'Bayraktar'
+age = 1.5
+# print('My name is {} {}'.format(name,surname))
+
+# print('My name is {1} {0}'.format(name,surname))
+
+# print('My name is {s} {n}'.format(n=name,s=surname))
+
+# print("My name is {s} {n} and I'am {a} years old.".format(n=name,s=surname,a=age))
+
+# result = 200/700
+
+# # burada r:1.4 için 1 tam kısım için kaç boşluk olacağı
+# # 4 ise noktadan sonra kaç hane olacağı 
+# print('the result is {r:1.4}'.format(r=result))
+
+print(f"My name is {name} {surname} and I'am {age} years old.")

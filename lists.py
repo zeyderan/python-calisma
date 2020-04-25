ad2b5645a099791fe80e380c2fa2f4ae359607fb
zeyderan <aykutbayraktar34gmail.com>
2020-04-25 13:38:43 +0300

    listeler konusu anlaşıldı

---
+++ lists.py
@@ -0,0 +1,15 @@
+list1 = ['one','two','three']
+list2 = ['four','five','six']
+
+# listeler + operatörü ile toplanabilir
+
+numbers = list1 + list2
+
+print(numbers)
+print(len(numbers))
+print(numbers[0])
+
+# elemanlarıda liste olan listeler oluşturmak için
+
+numbers = [list1,list2]
+print(numbers)

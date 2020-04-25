0548396935d65fedc98a6174ada87f49cec993da
zeyderan <aykutbayraktar34gmail.com>
2020-04-25 16:28:24 +0300

    liste metodları inceleniyor

---
+++ lists-metods-demo.py
@@ -0,0 +1,29 @@
+names = ['Ali','Yağmur','Hakan','Deniz']
+years = [1998,2000,1998,1987]
+
+# 1- 'Cenk' ismini listenin sonuna ekleyin
+names.append('Cenk')
+result = names
+# 2- 'Sena' değerini listenin başına ekleyin
+names.insert(0,'Sena')
+result = names
+# 3- 'Deniz' ismini listeden silin
+denizIndex = names.remove('Deniz')
+result = names
+# 4- 'Deniz' isminin indexi nedir
+#print(denizIndex)
+# 5- 'Ali' listenin bir elemanı mıdır ?
+result = 'Ali' in names
+# 6- Liste elemanlarını ters çevirin
+result = names[::-1]
+# 7- Liste elemanlarını alfabetik sıralayınız
+result = sorted(names)
+# 8- years listesini rakamsal büyüklüğe göre sıralayınız
+result = sorted(years)
+# 9- str = 'Chevrolet,Dacia' karakter dizisini listeye çevirin
+# 10- years dizisinin en büyük ve en küçük elemanı nedir
+# 11- years dizisinde kaç tane 1998 değeri vardır
+# 12- years dizisinin tüm elemanlarını silin
+# 13- Kullanıcıdan alacağınız 3 tae marka bilgisini bir listede saklayınız
+ 
+print(result)

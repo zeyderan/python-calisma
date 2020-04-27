cbf3eadbc81c53fa2e46f79687136c94a4b556a0
zeyderan <aykutbayraktar34gmail.com>
2020-04-27 15:07:44 +0300

    karşılaştırma operatörleri incelendi

---
+++ comparison-demo.py
@@ -0,0 +1,24 @@
+# 1- Girilen 2 sayıdan hangisi büyüktür ?
+# sayı1 = int(input('1. sayıyı girin : '))
+# sayı2 = int(input('2. sayıyı girin : '))
+
+# result = (sayı1 > sayı2)
+# print(f'ilk sayı :{sayı1} ikinci sayı :{sayı2} den büyüktür : {result}')
+
+# 2- Kullanıcıdan 2 vize (%60) ve final (%40) notu alıp ortalama hesaplayınız
+#   Eğer ortalama 50 ve üzeri ise geçti değilse kaldı yazdırın
+# vize1 = float(input('1. vize notu girin :'))
+# vize2 = float(input('2. vize notu girin :'))
+# final = float(input('final notu girin :'))
+# ortalama = (((vize1+vize2) / 2) * 0.6) + (final * 0.4)
+# print(f'not ortalamanız : {ortalama} ve derste geçme durumunuz : {ortalama >= 50}')
+
+# 3- Girilen bir sayının tek mi çiftmi olduğunu yazdırın
+a = int(input('sayı girin :'))
+print(f'girdiğiniz sayının çiftlik durumu : {(a % 2) == 0}')
+
+# 4- Girilen bir sayının negatif mi pozitif mi olduğunu yazdırın
+
+
+# 5- Parolaa ve email bilgisi isteyip doğruluğunu kontrol edin
+#   (email:email@gmail.com parola:abc123)
---
+++ comparison.py
@@ -0,0 +1,21 @@
+# username, password ==> database
+
+# 'aykut', '123456'
+
+a, b, c, d, = 5, 5, 10, 4
+password = '1234'
+username = 'aykut'
+result = (a == b)
+result = (a == c)
+
+result = ('aykt' == username)
+result = ('aykut' == username)
+
+result = (a != b)
+result = (a != c)
+
+result = (a > b)
+
+result = (a >= b)
+
+print(result)

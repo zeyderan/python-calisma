bab67aa67119645aac7f3efd88ae8a673cc0a789
zeyderan <aykutbayraktar34gmail.com>
2020-04-27 15:32:10 +0300

    mantıksal öperatörler işlendi

---
+++ logical.py
@@ -0,0 +1,16 @@
+x = 6
+hak = 5
+devam = 'e'
+result = 5 < x <10
+
+#and
+result = (x > 5) and (x < 10) # True,True => True
+result = (hak > 0) and (devam =='e')
+#or
+result = (x > 0) or (x % 2 == 0)
+
+#not
+result = not(x > 0)
+
+
+print(result)

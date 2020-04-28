74ccc06c3f3b28659f920ff2ca424087d06912fa
zeyderan <aykutbayraktar34@gmail.com>
2020-04-28 14:48:39 +0300

    for döngüsü işlendi

---
+++ for.py
@@ -0,0 +1,24 @@
+# numbers = [1,2,3,4,5]
+
+# for num in numbers:
+#     print(num)
+
+# names = ['aykut','sibel','ömer']
+
+# for name in names:
+#     print(f'my name is {name}')
+
+# name = 'aykut'
+
+# for n in name:
+#     print(n)
+
+# tuple = [(1,2),(3,4),(5,6)]
+
+# for t,z in tuple:
+#     print(t,z)
+
+d = {'k1':1,'k2':2,'k3':3}
+
+for key,value in d.items():
+    print(key,value)

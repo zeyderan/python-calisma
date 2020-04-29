d6ab35f4bf4247c5765102b4c72687f971ae145c
zeyderan <aykutbayraktar34@gmail.com>
2020-04-29 12:14:12 +0300

    döngü metodları inceledi

---
+++ loop-operators.py
@@ -0,0 +1,20 @@
+# range verile aralıkta verilen adım sayısı ile liste oluşturur
+
+# for item in range(50,100,20):
+#     print(item)
+
+
+# enumerate verilen liste için numaralandırma yapar
+
+# greeting = 'Hello There'
+
+# for index, letter in enumerate(greeting):
+#     print(f'index: {index} letter: {letter}')
+
+
+# zip iki listenin aynı indexli elemanlarını bir araya getirir
+
+# list1 = [1,2,3,4,5]
+# list2 = ['a','b','c','d','e']
+
+# print(list(zip(list1,list2)))

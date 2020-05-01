57579a389e9b398f2d47ec72f83c8f13ba9b0201
zeyderan <aykutbayraktar34@gmail.com>
2020-05-01 10:05:34 +0300

    fonksiyolar ve metodlar incelendi

---
+++ methods.py
@@ -0,0 +1,9 @@
+list = [1,2,3]
+print(type(list))
+
+myString = 'Hello'
+
+print(type(myString)) #metodlar class içerisinde tanımıdır
+print(myString.upper())
+
+# fonksiyon lar class içerisinde tanımlanmaz

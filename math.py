49f0d163efc3a402eaa50b2e6c40db92c25acdda
zeyderan <aykutbayraktar34@gmail.com>
2020-05-02 14:51:08 +0300

    modül kullanımı incelendi

---
+++ math.py
@@ -0,0 +1,17 @@
+# yöntem 1
+
+# import math
+# value = help(math.factorial)
+# print(value)
+
+# import math as islem
+
+# value = islem.factorial(5)
+# print(value)
+
+# yöntem 2
+
+from math import *
+
+value = factorial(5)
+print(value)

c31299ae135166188f1798c67a286bdb45a8df9e
zeyderan <aykutbayraktar34@gmail.com>
2020-05-02 11:32:28 +0300

    özel metodlara giriş yapıldı

---
+++ specials.py
@@ -0,0 +1,23 @@
+# mylist = [1,2,3]
+
+# print(len(mylist))
+
+class Movie():
+    def __init__(self,title , director, duration):
+        self.title = title
+        self.director = director
+        self.duration = duration
+        print('movie objesi oluşturuldu')
+    
+    def __str__(self):
+        return f'{self.title} by {self.director}'
+
+    def __len__(self):
+        return self.duration
+
+    def __del__(self):
+        print('film silindi')
+
+m = Movie('film adı','yönetmen adı',120)
+print(m)
+print(len(m))

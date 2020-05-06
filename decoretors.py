bc588a6a766e1991b3959775467dd9d9acc3fe44
zeyderan <aykutbayraktar34@gmail.com>
2020-05-07 01:03:52 +0300

    decorator fonk işlendi

---
+++ decoretors.py
@@ -0,0 +1,17 @@
+# def my_decorator(func):
+#     def wrapper():
+#         print('fonksiyondan önce olan işlemler')
+#         func()
+#         print('fonksiyondan sonra olan işlemler')
+#     return wrapper
+
+# @my_decorator
+# def sayHello():
+#     print('hello')
+
+
+# # sayHello = my_decorator(sayHello) #burada sayHelloya decorator içerisindeki özellikleri ekliyoruz
+# # sayHello() # @my_decorator ile kısayoldan bunu yaparız
+
+# kod tekrarının önüne geçmek için decorator kullanmak çok önemlidir
+12e

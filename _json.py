53c2348d244673ac1b554487fde58dcf25418c98
zeyderan <aykutbayraktar34@gmail.com>
2020-05-14 17:31:50 +0300

    json incelenmesi tamamlandı.

--- _json.py
+++ _json.py
@@ -8,12 +8,27 @@ import json
 #json.decoder.JSONDecodeError: Expecting property name enclosed in double quotes
 person_string = '{"name":"Ali","languages":["python","C#"]}'
 
-# JSON string to Dict
-result = json.loads(person_string)
+# # JSON string to Dict
+# result = json.loads(person_string)
 
-with open('newfile.txt') as f:
-    data = json.load(f)
-    print('from file')
-    print(data['name'])
-print('from person_string variable')  
-print(result['name'])
+# with open('newfile.txt') as f:
+#     data = json.load(f)
+#     print('from file')
+#     print(data['name'])
+# print('from person_string variable')  
+# print(result['name'])
+
+
+# # Dict to Json String
+person_dict = {
+    "name": "Aykut",
+    'languages': ["Python","C#"]
+}
+
+# result = json.dumps(person_dict)
+# print(result)
+# print(type(result))
+
+with open("newfile.txt","w") as f:
+    json.dump(person_dict,f)
+

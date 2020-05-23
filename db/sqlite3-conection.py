b972c39f49f99a5f8eb5834bd4928cb66a489d58
zeyderan <aykutbayraktar34@gmail.com>
2020-05-23 13:13:04 +0300

    sqlite3 db ile in memory db çalışması yapıldı

--- db/sqlite3-conection.py
+++ db/sqlite3-conection.py
@@ -1,1 +1,25 @@
-import sq
+import sqlite3 as db
+
+mydb = db.connect(':memory:') # in memory db kod çalışınca var olur kod son bulunca hafızadan silinir
+my_cursor = mydb.cursor() # imleç
+
+# yeni bir tablo oluştur
+my_cursor.execute('''\ 
+                CREATE TABLE isim_soyisim (isim, soyisim)\
+''')
+# işlemi uygula (her seferinde commit etmeye gerek yok)
+mydb.commit()
+
+# tabloya veri ekle
+my_cursor.execute('''\
+                INSERT INTO isim_soyisim VALUES ('AYKUT','BAYRAKTAR')\
+''')
+
+mydb.commit()
+
+# eklediğin verileri tablodan çek
+data = my_cursor.execute('''\
+                SELECT * FROM isim_soyisim\
+''').fetchall()
+
+print(data)
